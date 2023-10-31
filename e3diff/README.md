d10d3ed37f752dade1b1bdaee3e875495b502db1
horoiwa <horoiwa195@gmail.com>
2023-10-31 23:12:07 +0900

    sort

---
+++ e3diff/README.md
@@ -0,0 +1,8 @@
+
+
+## Requirements
+
+```
+python==3.11.5
+tensorflow==2.13.1
+```
---
+++ e3diff/main.py
@@ -0,0 +1,18 @@
+
+
+def prepare_dataset():
+    pass
+
+
+def train():
+    pass
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    prepare_dataset()
+    train()
+    test()
